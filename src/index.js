d065334df1f11f74d155af55a3222aee3f765be7
himanshu.gautam@berylsystems.com <himanshu.gautam@berylsystems.com>
2021-02-12 17:16:58 +0530

    ReatJs 1st Project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import Avatar from './Avatar'
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <div>
+  <Avatar/>
+   </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
